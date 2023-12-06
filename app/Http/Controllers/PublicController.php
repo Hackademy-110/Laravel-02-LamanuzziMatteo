1e78d26165cf53cdfe54e0d053bc1af0c97c6e0b
Matteo <lamanuzzi.matteo@gmail.com>
2023-12-06 16:36:25 +0100

    aggiustato array

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller{
+    public $pool=[
+        ['nome'=>'Minigheddu','img'=>'/Media/Merc1.jpg'],
+        ['nome'=>'Passagareddu','img'=>'/Media/Merc2.jpg'],
+        ['nome'=>'Franco','img'=>'/Media/Merc3.jpg'],
+        ['nome'=>'Paoleddu','img'=>'/Media/Merc4.jpg'],
+
+    ];
     public function Welcome(){
         return view('welcome');
     }
@@ -12,33 +19,11 @@ class PublicController extends Controller{
         return view('personale');
     }
     public function Mercenary(){
-        $pool=[
-            ['nome'=>'Minigheddu','img'=>'/Media/Merc1.jpg'],
-            ['nome'=>'Passagareddu','img'=>'/Media/Merc2.jpg'],
-            ['nome'=>'Franco','img'=>'/Media/Merc3.jpg'],
-            ['nome'=>'Paoleddu','img'=>'/Media/Merc4.jpg'],
-
-        ];
-    //     $Empty=['nome'=>'Igor','img'=>'/Media/Casa.jpg'];
-    //     foreach($pool as $single){
-    //     if($recruit==$single['name']){
-    //         return view('Mercenario',['Scelto'=>$single]);
-    //     }
-    // }
-    // return view('Mercenario',['Scelto'=>$Empty]);
-    // }
-    return view('mercenario',['pool'=>$pool]);
+    return view('mercenario',['pool'=>$this->pool]);
 }
 public function Selezionato($Scelto){
-    $pool=[
-        ['nome'=>'Minigheddu','img'=>'/Media/Merc1.jpg'],
-        ['nome'=>'Passagareddu','img'=>'/Media/Merc2.jpg'],
-        ['nome'=>'Franco','img'=>'/Media/Merc3.jpg'],
-        ['nome'=>'Paoleddu','img'=>'/Media/Merc4.jpg'],
-
-    ];
     $Empty=['nome'=>'Igor','img'=>'/Media/Casa.jpg'];
-    foreach($pool as $single){
+    foreach($this->pool as $single){
     if($Scelto==$single['nome']){
         return view('Chosen',['theone'=>$single]);
     }
